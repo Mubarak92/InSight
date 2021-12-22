670333d7bac0e14967918e1ac0869177aa0ea136
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-12-22 23:14:55 +0300

    Authentication

--- app/src/main/java/com/example/insight/LoginPage.kt
+++ app/src/main/java/com/example/insight/LoginPage.kt
@@ -1,10 +1,98 @@
 package com.example.insight
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.insight.databinding.FragmentLoginPageBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 
 class LoginPage : Fragment() {
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+    )
+    // Create and launch sign-in intent
+    val signInIntent = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
+        private lateinit var auth: FirebaseAuth
+        lateinit var googleSignInClient: GoogleSignInClient
+
+        private var _binding: FragmentLoginPageBinding? = null
+        private val binding get() = _binding
 
 
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
+            return binding?.root
+        }
 
-}
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.get_key))
+                .requestEmail().build()
+
+            googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+            auth = Firebase.auth
+//binding?.signOut?.setOnClickListener {
+//
+//}
+            binding?.login?.setOnClickListener {
+                signInLauncher.launch(signInIntent)
+
+                    findNavController().navigate(R.id.action_loginPage_to_mainPage)
+            }
+
+            auth.currentUser?.displayName
+
+
+        }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
+
+    }
+
+
